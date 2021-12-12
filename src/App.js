43d08da1b8495b4259624d77bf620df8b413a1b2
Dmitriy Ovsienko <ovsienkodima@mail.ru>
2021-12-12 21:06:09 +0300

    last

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState} from 'react';
+import {EditorState} from "draft-js";
+import {Editor} from "react-draft-wysiwyg";
+import 'draft-js/dist/Draft.css';
 
 function App() {
+  const [editorState, setEditorState] = React.useState(
+    () => EditorState.createEmpty(),
+  );
+ 
+  console.log(editorState)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>React Wysiwyg Rich Editor Using Draft.js</h2>
+        <Editor 
+          editorState={editorState}
+          // onChange={setEditorState}
+        />
+      </div>
     </div>
   );
 }
